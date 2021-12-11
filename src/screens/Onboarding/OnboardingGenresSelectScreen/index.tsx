eba456464b767af80cf1f5a3459a1fbbd87454a2
Federico Giorgino <federicog.dev@gmail.com>
2021-12-11 18:36:16 +0100

    OnboardingGenresSelectionScreen

--- src/screens/Onboarding/OnboardingGenresSelectScreen/index.tsx
+++ src/screens/Onboarding/OnboardingGenresSelectScreen/index.tsx
@@ -1,13 +1,59 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { useTheme } from "@react-navigation/native";
+import React, { useContext } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
+import GenresSelectionTile from "../../../components/GenresSelectionTile";
+import Spacer from "../../../components/Spacer";
+import Typography from "../../../components/Typography";
+
+import { SettingsContext } from "../../../context/SettingsContext";
 import { styles } from "./styles";
 
 const OnboardingGenresSelectScreen = () => {
+  const { genres, selectedGenres, setFirstVisitFalse } =
+    useContext(SettingsContext);
+  const { colors } = useTheme();
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView style={styles.page}>
+      <View style={styles.header}>
+        <Typography>Also let us know what kind of games you like.</Typography>
+      </View>
+      <Spacer y={20} />
+      <View style={styles.content}>
+        <FlatList
+          data={genres
+            .sort(
+              (a, b) =>
+                selectedGenres.indexOf(b.id) - selectedGenres.indexOf(a.id)
+            )
+            .sort((a, b) => a.name.localeCompare(b.name))}
+          renderItem={({ item }) => <GenresSelectionTile genre={item} />}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </View>
+      <View style={styles.buttonWrapper}>
+        <TouchableOpacity
+          disabled={selectedGenres.length < 1}
+          style={[
+            styles.button,
+            {
+              backgroundColor:
+                selectedGenres.length < 1 ? "grey" : colors.primary,
+            },
+          ]}
+          onPress={() => setFirstVisitFalse()}
+        >
+          <Text style={styles.buttonText}>Done</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- src/screens/Onboarding/OnboardingGenresSelectScreen/styles.ts
+++ src/screens/Onboarding/OnboardingGenresSelectScreen/styles.ts
@@ -1,3 +1,14 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  page: { flex: 1 },
+  header: { paddingHorizontal: 15 },
+  content: { flex: 1 },
+  buttonWrapper: {
+    flex: 1 / 6,
+    justifyContent: "center",
+    padding: 15,
+  },
+  buttonText: { color: "#fff", textAlign: "center", fontWeight: "700" },
+  button: { borderRadius: 55, padding: 15 },
+});
