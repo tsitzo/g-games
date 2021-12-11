81c5ab4c02bfb0df998bcb2bd84630bec26fd0a8
Federico Giorgino <federicog.dev@gmail.com>
2021-12-11 18:29:49 +0100

    OnboardingLandingScreen

--- src/screens/Onboarding/OnboardingLandingScreen/index.tsx
+++ src/screens/Onboarding/OnboardingLandingScreen/index.tsx
@@ -1,13 +1,52 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { useTheme } from "@react-navigation/native";
+import React, { FC } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import Typography from "../../../components/Typography";
 
 import { styles } from "./styles";
+import Spacer from "../../../components/Spacer";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { AppStackParams } from "../../../types";
 
-const OnboardingLandingScreen = () => {
+interface IOnboardingLandingScreenProps {
+  navigation: NativeStackNavigationProp<
+    AppStackParams,
+    "OnboardingLandingScreen"
+  >;
+}
+
+const OnboardingLandingScreen: FC<IOnboardingLandingScreenProps> = ({
+  navigation,
+}) => {
+  const { colors } = useTheme();
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView style={styles.centeredPage}>
+      <Image
+        source={require("../../../../assets/videogame.png")}
+        style={styles.image}
+      />
+      <Spacer y={20} />
+
+      <Typography variant="bold" size={22}>
+        Welcome to GGames
+      </Typography>
+      <Spacer y={20} />
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: colors.primary }]}
+        onPress={() => navigation.push("OnboardingPlatformsSelectScreen")}
+      >
+        <Text style={styles.buttonText}>Let's go </Text>
+        <Ionicons name="arrow-forward-outline" size={24} color="#fff" />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
--- src/screens/Onboarding/OnboardingLandingScreen/styles.ts
+++ src/screens/Onboarding/OnboardingLandingScreen/styles.ts
@@ -1,3 +1,16 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  centeredPage: { flex: 1, justifyContent: "center", alignItems: "center" },
+  image: { height: 150, width: 150 },
+
+  button: {
+    padding: 10,
+    paddingHorizontal: 50,
+    borderRadius: 55,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: { fontWeight: "700", color: "#fff", fontSize: 18 },
+});
