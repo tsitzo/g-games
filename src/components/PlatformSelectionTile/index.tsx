8a9d247d9d05879c819b0bc1ac5c7fb5b30e1667
Federico Giorgino <federicog.dev@gmail.com>
2021-12-11 18:32:52 +0100

    PlatformSelectionTile

---
+++ src/components/PlatformSelectionTile/index.tsx
@@ -0,0 +1,48 @@
+import React, { FC, useContext } from "react";
+import { useTheme } from "@react-navigation/native";
+import { TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import Typography from "../Typography";
+import { SettingsContext } from "../../context/SettingsContext";
+import { SettingsPlatform } from "../../types";
+import { styles } from "./styles";
+
+interface IPlatformSelectionTileProps {
+  platform: SettingsPlatform;
+}
+
+const PlatformSelectionTile: FC<IPlatformSelectionTileProps> = ({
+  platform,
+}) => {
+  const { selectedPlatforms, addPlatform, removePlatform } =
+    useContext(SettingsContext);
+
+  const { colors } = useTheme();
+
+  const selected = selectedPlatforms.find((p) => p === platform.id);
+
+  return (
+    <TouchableOpacity
+      style={styles.tile}
+      onPress={() =>
+        selected ? removePlatform(platform) : addPlatform(platform)
+      }
+    >
+      <View style={styles.tileLeft}>
+        <Typography
+          variant={selected ? "bold" : "light"}
+          size={16}
+          numberOfLines={1}
+        >
+          {platform.name}
+        </Typography>
+      </View>
+      {selected && (
+        <Ionicons name="md-checkmark-sharp" size={16} color={colors.primary} />
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default PlatformSelectionTile;
---
+++ src/components/PlatformSelectionTile/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  tile: {
+    paddingHorizontal: 15,
+    marginVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  tileLeft: {
+    width: "90%",
+  },
+});
