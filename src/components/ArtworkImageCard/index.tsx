2f7dbaba7dcac56ccbb6fe6fda6997b73879a21d
Federico Giorgino <federicog.dev@gmail.com>
2021-12-13 04:02:11 +0100

    ArtworkImageCard

---
+++ src/components/ArtworkImageCard/index.tsx
@@ -0,0 +1,43 @@
+import { useTheme } from "@react-navigation/native";
+import React, { FC, useState } from "react";
+import { View, Image, ActivityIndicator } from "react-native";
+import { Game } from "../../types";
+
+import { styles } from "./styles";
+
+interface IArtworkImageCard {
+  game: Game;
+}
+
+const ArtworkImageCard: FC<IArtworkImageCard> = ({ game }) => {
+  const { colors } = useTheme();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  return (
+    <View style={styles.imageWrapper}>
+      {isLoading && (
+        <View
+          style={[styles.placeHolderImage, { backgroundColor: colors.card }]}
+        >
+          <ActivityIndicator color={colors.primary} />
+        </View>
+      )}
+      <Image
+        resizeMode="cover"
+        source={{
+          uri: game?.artworks
+            ? `https:${game.artworks[0].url?.replace(
+                "t_thumb",
+                "t_screenshot_big_2x"
+              )}`
+            : `https:${game?.cover?.url?.replace("t_thumb", "t_cover_big_2x")}`,
+        }}
+        style={styles.image}
+        onLoadStart={() => setIsLoading(true)}
+        onLoadEnd={() => setIsLoading(false)}
+      />
+    </View>
+  );
+};
+
+export default ArtworkImageCard;
