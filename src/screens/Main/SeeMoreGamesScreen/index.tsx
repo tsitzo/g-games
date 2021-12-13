178287d935fe3ab8312b9a2ad9305f0fcd3ea62f
Federico Giorgino <federicog.dev@gmail.com>
2021-12-13 04:34:46 +0100

    SeeMoreGamesScreen

--- src/screens/Main/SeeMoreGamesScreen/index.tsx
+++ src/screens/Main/SeeMoreGamesScreen/index.tsx
@@ -1,13 +1,49 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { RouteProp } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import React, { FC } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import CoverImageCard from "../../../components/CoverImageCard";
+import GridCoverImageCard from "../../../components/GridCoverImageCard";
+import { AppStackParams } from "../../../types";
 
 import { styles } from "./styles";
 
-const SeeMoreGamesScreen = () => {
+interface ISeeMoreGamesScreenProps {
+  navigation: NativeStackNavigationProp<AppStackParams, "SeeMoreGamesScreen">;
+  route: RouteProp<AppStackParams, "SeeMoreGamesScreen">;
+}
+
+const SeeMoreGamesScreen: FC<ISeeMoreGamesScreenProps> = ({
+  route,
+  navigation,
+}) => {
+  const { games } = route.params;
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        style={styles.flatList}
+        numColumns={3}
+        data={games}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("GameDetailsScreen", {
+                id: item.id,
+              })
+            }
+            style={styles.imageWrapper}
+          >
+            <GridCoverImageCard game={item} />
+          </TouchableOpacity>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
--- src/screens/Main/SeeMoreGamesScreen/styles.ts
+++ src/screens/Main/SeeMoreGamesScreen/styles.ts
@@ -1,3 +1,12 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  flatList: { paddingHorizontal: 7.5 },
+  imageWrapper: {
+    flex: 1 / 3,
+    margin: 7.5,
+    aspectRatio: 135 / 195,
+    overflow: "hidden",
+    borderRadius: 5,
+  },
+});
